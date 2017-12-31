a04d3e7e79891da52319d89bc50768957a7bc3d6
Island0214 <549378813@qq.com>
2017-12-31 21:29:28 +0800

    <feat> : <add frame of proModelCollections page>

---
+++ src/components/ProModelCollections/ProModelCollections.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="model-collections-wrapper">
+    <el-row :gutter="20" style="margin: 0;">
+      <el-col :xs="24" :sm="24" :md="12" :lg="12">
+        <div class="breadcrumb-wrapper">
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>专业上传</el-breadcrumb-item>
+            <el-breadcrumb-item>模型文件夹
+              <el-tooltip content="上传代码文件夹以供训练选用" placement="bottom" effect="light">
+                <i class="el-icon-question"></i>
+              </el-tooltip>
+            </el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="12">
+        <div :class="[largeSize ? 'upload-button-wrapper-lg' : 'upload-button-wrapper-sm' ]">
+          <my-button content="上传文件夹" color="#000000"></my-button>
+        </div>
+      </el-col>
+    </el-row>
+
+    <div class="body-wrapper">
+      <el-row :gutter="20" style="margin: 0;">
+        <el-col>
+          <div class="collection-wrapper">
+            <div class="name-wrapper">
+              <h3>模型<br>文件夹1</h3>
+
+            </div>
+            <div class="buttons-wrapper">
+
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+  import MyButton from '../Basic/MyButton/MyButton.vue'
+  import { mapGetters } from 'vuex'
+
+  export default {
+    components: {
+      MyButton
+    },
+    data () {
+      return {
+
+      }
+    },
+    computed: {
+      ...mapGetters({
+        largeSize: 'largeSize'
+      })
+    }
+  }
+</script>
+
+<style scoped src="./ProModelCollections.css"></style>
